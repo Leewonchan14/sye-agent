8b712e9c160f764912be2ab74c0075350c75e656
WonChan Lee <twoone14@tukorea.ac.kr>
2026-07-16 15:54:36 +0900

    feat: 44 commit

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,12 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {/* config options here */};
+const nextConfig: NextConfig = {
+  outputFileTracingIncludes: {
+    "/*": [
+      "./node_modules/garu-ko/pkg/*.wasm",
+      "./node_modules/garu-ko/models/*.gmdl",
+    ],
+  },
+};
 
 export default nextConfig;
